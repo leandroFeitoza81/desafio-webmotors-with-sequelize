ad89fbc7db4a0cd994e5dd2172048cba54f06a7d
Leandro <leandro.feitoza@gmail.com>
2021-09-06 23:16:33 -0300

    Implantado teste na rota PUT

--- __tests__/updateAnnouncement.test.js
+++
@@ -1,5 +0,0 @@
-const { it, expect, beforeEach, describe } = require('@jest/globals');
-const request = require('supertest');
-const app = require('../src/app');
-const truncate = require('./utils/truncate');
-const { tb_AnuncioWebmotors } = require('../src/models');
